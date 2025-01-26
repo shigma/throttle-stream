298c1a4a2cede739020513a0090be4e40f8147f2
Shigma <shigma10826@gmail.com>
2025-01-26 18:06:05 +0800

    feat: support flush

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,15 @@ pub struct ThrottleStream<T> {
 }
 
 impl<T: Send + 'static> ThrottleStream<T> {
+    pub fn clone(&self) -> Self {
+        Self {
+            duration: self.duration,
+            buffer: self.buffer.clone(),
+            task: self.task.clone(),
+            sender: self.sender.clone(),
+        }
+    }
+
     pub fn new(duration: Duration) -> (Self, mpsc::Receiver<Vec<T>>) {
         let (sender, receiver) = mpsc::channel(1);
         let buffer = Arc::new(Mutex::new(Vec::new()));
@@ -25,15 +34,6 @@ impl<T: Send + 'static> ThrottleStream<T> {
             },
             receiver,
         )
-    }
-
-    pub fn clone(&self) -> Self {
-        Self {
-            duration: self.duration,
-            buffer: self.buffer.clone(),
-            task: self.task.clone(),
-            sender: self.sender.clone(),
-        }
     }
 
     fn schedule_task(&self) -> JoinHandle<()> {
@@ -71,6 +71,21 @@ impl<T: Send + 'static> ThrottleStream<T> {
             *task_guard = Some(self.schedule_task());
         }
     }
+
+    pub async fn flush(&self) {
+        let mut buffer_guard = self.buffer.lock().await;
+        let mut task_guard = self.task.lock().await;
+        if buffer_guard.is_empty() {
+            return;
+        }
+
+        let data = buffer_guard.drain(..).collect::<Vec<_>>();
+        if let Err(e) = self.sender.send(data).await {
+            eprintln!("Failed to send data: {}", e);
+            return;
+        }
+        *task_guard = Some(self.schedule_task());
+    }
 }
 
 #[cfg(test)]
@@ -102,5 +117,36 @@ mod tests {
         sleep(Duration::from_millis(120)).await;
         // time: 240ms
         assert_eq!(receiver.recv().await, Some(vec![4, 5]));
+    }
+
+    #[tokio::test]
+    async fn test_throttle_stream_flush() {
+        let (throttle, mut receiver) = ThrottleStream::new(Duration::from_millis(100));
+
+        throttle.push(1).await;
+        // time: 0ms
+        assert_eq!(receiver.try_recv(), Ok(vec![1]));
+
+        throttle.push(2).await;
+        throttle.push(3).await;
+        // time: 0ms
+        assert!(receiver.try_recv().is_err());
+
+        throttle.flush().await;
+        // time: 0ms
+        assert_eq!(receiver.try_recv(), Ok(vec![2, 3]));
+
+        throttle.flush().await;
+        // time: 0ms
+        assert!(receiver.try_recv().is_err());
+
+        throttle.push(4).await;
+        throttle.push(5).await;
+        // time: 0ms
+        assert!(receiver.try_recv().is_err());
+
+        throttle.flush().await;
+        // time: 0ms
+        assert_eq!(receiver.try_recv(), Ok(vec![4, 5]));
     }
 }
